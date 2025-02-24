7c0e2eb5356adaee82c8bf6a8e35c9f0b65098e1
arummulya <tugasarum01@gmail.com>
2025-02-24 23:46:59 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
   <h1>Hello world!</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum exercitationem at maiores quod aperiam accusantium sit modi consequatur laborum in architecto placeat, aspernatur recusandae soluta. Modi minima velit assumenda corrupti.
 </body>
 </html>
